32a93a29407203bb69358887a2b2cef69dd49746
unknown <alves.vn@gmail.com>
2022-07-13 08:11:10 -0300

    inserido o arquivo html e js

---
+++ script.js
@@ -0,0 +1,20 @@
+function fizzbuzz() {
+    let i = 0;
+    let temp = [];
+    let fizz, buzz = "";
+    let params = prompt("Digite um número para percorrer do 1 até ele:");
+    while (i <= params) {
+        if (i%3 === 0 && i%5 === 0) {
+            temp[i-1] = "FizzBuzz";
+        } else if (i%3 === 0) {
+            temp[i-1] = "Fizz";
+        } else if (i%5 === 0) {
+            temp[i-1] = "Buzz";
+        } else{
+            temp[i-1] = i;
+        }
+        
+        i++;
+    }
+    return document.getElementById("demo").innerHTML = temp;
+}
